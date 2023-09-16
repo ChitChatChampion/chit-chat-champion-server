0f777c9918b527e8ea4bdec88cd36b1be8b2658c
Neo Jing Xuan <neojxuan@gmail.com>
2023-09-17 01:38:17 +0800

    add CORS for convenient development

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # backend
 
 ## Setup
-1. Run `python3 -m venv env`
+1. Run `python3 -m venv env` to create a virtual environment called "env"
 2. Run `source env/bin/activate` to change your environment
 3. `pip install -r requirements.txt` to install dependencies
 
--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 # encoding: utf-8
 import json
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) # change for production to only IP address of front-end app
 
 @app.route('/', methods=['GET'])
 def query_records():
@@ -14,8 +16,6 @@ def query_records():
 @app.route('/room/csc', methods=["POST"])
 def create_csc_room():
     response = jsonify()
-
-    
 
     return response
 

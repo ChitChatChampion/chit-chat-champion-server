dcf00f8c3514792344d9fd693855e491254a5267
Neo Jing Xuan <neojxuan@gmail.com>
2023-09-25 03:47:58 +0800

    configure cors to allow frontend to send requests

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 import os
 from quart import Quart
+from quart_cors import cors
 from motor.motor_asyncio import AsyncIOMotorClient
 import openai
 import logging
@@ -21,6 +22,13 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 app.config['MONGODB_URI'] = os.getenv("MONGODB_URI")
 app.config['MODEL'] = MODEL
+
+# Configure CORS policies
+# TODO: update this to only allow requests from the frontend app
+app = cors(
+    app,
+    allow_origin="*"
+)
 
 # Create an AsyncIOMotorClient within the app context
 @app.before_serving

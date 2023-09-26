f5f925c5bb92f2c9abfc6d6f5cd26eb2d418e977
Neo Jing Xuan <neojxuan@gmail.com>
2023-09-26 14:48:41 +0800

    feat: add prompts for burning bridges

--- prompts/prompts.py
+++ prompts/prompts.py
@@ -11,3 +11,18 @@ assistant_example_csc = """
     "What was your favourite experience in a hackathon like?"
 ]
 """
+
+user_example_bb = """
+The participants in this game are Strangers, and the purpose of the game is for a school of computing orientation. \
+Other information about the participants is that: they are fun loving people. \
+I want you to generate questions where the answer must be one of the players and players would not want to be chosen. \
+The number of questions I want you to generate is 3.
+"""
+
+assistant_example_bb = """
+[
+    "In a hackathon, who would you least want as your coding partner?",
+    "If we had to rely on someone to fix a critical server issue, who would probably make things worse instead of better?",
+    "Who do you think would be the most likely to accidentally commit confidential information into a public code repository?"
+]
+"""
